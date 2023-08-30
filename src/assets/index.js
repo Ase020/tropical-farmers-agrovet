1ca988deae8b7210db580b349266e2dd6bf21e59
brianrono <brianrono16@gmail.com>
2023-08-31 02:13:45 +0300

    Clean assets code

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import facebook from "./facebook.png";
+import whatsApp from "./whatsapp.png";
+import twitter from "./twitter.png";
+import instagram from "./instagram.png";
+import youtube from "./youtube.png";
+import tropicalLogo from "./tropical-logo.png"
+
+export {
+    facebook, 
+    whatsApp, 
+    twitter, 
+    instagram, 
+    youtube,
+    tropicalLogo,
+};
