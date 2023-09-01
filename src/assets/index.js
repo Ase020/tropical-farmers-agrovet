d8fdb241d46321ac685af9f6eced2f4c26e98cff
brianrono <brianrono16@gmail.com>
2023-09-01 23:21:55 +0300

    Build footer assets file

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,7 @@
-import facebook from "./facebook.png";
-import whatsApp from "./whatsapp.png";
-import twitter from "./twitter.png";
-import instagram from "./instagram.png";
-import youtube from "./youtube.png";
-import tropicalLogo from "./tropical-logo.png"
+import tropicalLogo from "./tropical-logo.png";
+import sendMail from "./sendMail.png";
 
 export {
-    facebook, 
-    whatsApp, 
-    twitter, 
-    instagram, 
-    youtube,
     tropicalLogo,
+    sendMail,
 };
