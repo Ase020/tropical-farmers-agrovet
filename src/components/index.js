50cd56ad29cadeeaaf5930737cbe615ad7f806e3
brianrono <brianrono16@gmail.com>
2023-09-05 22:12:24 +0300

    Products set-up

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import { default as Navbar } from "./navbar/Navbar";
 import { default as Footer } from "./footer/Footer";
 import { default as HeroSection } from "./hero-section/HeroSection";
+import { default as Products } from "./products-page/Products";
 
-export { Navbar, Footer, HeroSection };
+export { Navbar, Footer, HeroSection, Products };
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
-import { Footer, Navbar } from "../components";
+import { Footer, Navbar, Products } from "../components";
 
 const Layout = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Outlet />
+    <>
+      <div className="app">
+        <Navbar />
+        <Outlet />
+      </div>
+      <Products />
       <Footer />
-    </div>
+    </>
   );
 };
 
