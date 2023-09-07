aeaabc42eda3a44ad78ecf7b402c4bbfe420ed67
felixNyalenda <felixasejunior@gmail.com>
2023-09-08 02:52:44 +0300

    product card build

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,7 +1,22 @@
 import "./product-card.css";
 
 const ProductCard = () => {
-  return <div className="product_card">Product Card</div>;
+  return (
+    <div className="product_card">
+      <div className="product_cover">
+        <img
+          src="/livestock_feeds.jpg"
+          alt="feeds"
+          className="product_cover-img"
+        />
+      </div>
+
+      <div className="product_info">
+        <h1 className="product_header">Animal Feeds</h1>
+        <button className="product_btn">View</button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
