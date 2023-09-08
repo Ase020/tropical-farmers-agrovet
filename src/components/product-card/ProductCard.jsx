d3666c58e84eef39ee6cb1d00158d784cf938ee0
felixNyalenda <felixasejunior@gmail.com>
2023-09-08 03:47:35 +0300

    product card fetch

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,18 +1,15 @@
+/* eslint-disable react/prop-types */
 import "./product-card.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <div className="product_card">
       <div className="product_cover">
-        <img
-          src="/livestock_feeds.jpg"
-          alt="feeds"
-          className="product_cover-img"
-        />
+        <img src={product.img} alt="feeds" className="product_cover-img" />
       </div>
 
       <div className="product_info">
-        <h1 className="product_header">Animal Feeds</h1>
+        <h1 className="product_header">{product.name}</h1>
         <button className="product_btn">View</button>
       </div>
     </div>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,13 +2,23 @@ import ProductCard from "../product-card/ProductCard";
 import "./products.css";
 
 const Products = () => {
+  const products = [
+    { id: 1, name: "Animal Feeds", img: "/livestock_feeds.jpg" },
+    { id: 2, name: "Vegetables Seeds", img: "/vegetable_seeds.jpeg" },
+    { id: 3, name: "Farm Inputs", img: "/livestock_feeds.jpg" },
+    { id: 4, name: "Veterinary Products", img: "/livestock_feeds.jpg" },
+    { id: 5, name: "Foliar", img: "/livestock_feeds.jpg" },
+    { id: 6, name: "Herbicides", img: "/livestock_feeds.jpg" },
+    { id: 7, name: "Nutritional Supplements", img: "/livestock_feeds.jpg" },
+    { id: 8, name: "Fertilizers", img: "/livestock_feeds.jpg" },
+  ];
   return (
     <div className="products">
       <div className="products_container">
         <h2 className="products_header">Our Products Range</h2>
         <div className="products_card-wrapper">
-          {Array.from({ length: 8 }, (_, i) => (
-            <ProductCard key={i} />
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
       </div>
