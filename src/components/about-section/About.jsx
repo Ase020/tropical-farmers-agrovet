17b162db75d547d0027cfef778bae0746ef7835f
brianrono <brianrono16@gmail.com>
2023-09-11 23:58:00 +0300

    About section partial build

---
+++ src/components/about-section/About.jsx
@@ -0,0 +1,49 @@
+// import React from 'react';
+import './about.css';
+
+function AboutSection() {
+    return (
+        <div className="about-section">
+            <div className="circle"></div>
+            <div className="content">
+                <h2>ABOUT US</h2>
+                <p>
+                    Tropical Farmers Agrovet Ltd Co Ltd is a Private Limited Company
+                    established under the Company&apos;s Act 2015, Laws of Kenya registered 5th
+                    February 2021 to carry out agriculture related business like Crop
+                    farming, Livestock Farming, Trading of agricultural products and other
+                    agriculture related business. The company started operation in 2021 to
+                    date. The Company is Located in Kericho County Kericho town along
+                    Kenyatta Street next to Kenya Highlands Sacco.
+                </p>
+            </div>
+            <div className="connector"></div>
+            <div className="content">
+                <h2>ROLE</h2>
+                <p>
+                    Our role in this market is to ensure that smallholder farmers are
+                    adequately linked to the market where they can reap good returns from
+                    their produce, and reduce post-harvest losses.
+                </p>
+            </div>
+            <div className="connector"></div>
+            <div className="content">
+                <h2>Vision</h2>
+                <p>
+                    To partner with both governmental and non-governmental entities in
+                    order to achieve food security and sustainability.
+                </p>
+            </div>
+            <div className="connector"></div>
+            <div className="content">
+                <h2>Mission</h2>
+                <p>
+                    To provide the best practices in the industry that guarantees quality
+                    and safe farm produce, and improve the lives of farmers.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutSection;
