2c8d57fb75bb0e0c0905fa49509b725aedd15474
brianrono <brianrono16@gmail.com>
2023-09-14 18:48:34 +0300

    Add more partner logos

---
+++ src/assets/partner-logos/index.js
@@ -0,0 +1,17 @@
+import bidcoLogo from "./bidco-logo.png";
+import kenyaSeed from "./kenya-seed.png";
+import modernWays from "./modernways.png";
+import pembeFeeds from "./pembefeeds3.png"
+import sigma from "./SIGMA_LOGO2.png";
+import ungaFarmcare from "./ungafarmcare.png";
+import vital from "./vital.png";
+
+export {
+    bidcoLogo,
+    kenyaSeed,
+    modernWays,
+    pembeFeeds,
+    sigma,
+    ungaFarmcare,
+    vital,
+};
